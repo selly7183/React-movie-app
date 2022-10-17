2f828577cc84328bb5d1ac89cb7489b076e68c3a
selly7183 <selly7183@naver.com>
2022-10-17 23:24:56 +0900

    routes수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-	},
-	{
-		path: "/movie/:id",
-		element: <Detail />,
-	},
-]);
+import Detail from "./routes/Detail";
 
 function App() {
-	return <RouterProvider router={router} />;
+	return (
+		<Router basename={process.env.PUBLIC_URL}>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+			<Routes>
+				<Route path="/movie/:id" element={<Detail />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
